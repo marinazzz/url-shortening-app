81ede57563989a4bf728126d8bfcbb15f362eb3c
Marina Zujic <zujicmarina@gmail.com>
2020-08-09 21:03:52 +0200

    Simplified markup for form, variables file included in custom-variables,changed all units to rem

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
   // place your code here
+
+  function checkSearchEmpty() {
+    $('#search').val(function() {
+      if($(this).val() === ''){
+        $('.invalid-feedback').css('display', 'block');
+      } return;
+    })
+  }
+
+  $('#submitBtn').click(function(e) {
+    e.preventDefault();
+    checkSearchEmpty();
+  })
+
 });
