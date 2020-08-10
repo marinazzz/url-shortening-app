8be5f7f86d4cc1ef4be2d42c7ec91fe127dc422b
Marina Zujic <zujicmarina@gmail.com>
2020-08-10 16:05:55 +0200

    Form validation

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 $(document).ready(function () {
-  // place your code here
 
-  function checkSearchEmpty() {
-    $('#search').val(function() {
-      if($(this).val() === ''){
-        $('.invalid-feedback').css('display', 'block');
-      } return;
-    })
-  }
+  const $search = $('#search');
 
   $('#submitBtn').click(function(e) {
     e.preventDefault();
     checkSearchEmpty();
+    checkUrlValid();
   })
 
+  let $searchVal = false;
+  function checkSearchEmpty() {
+      if($search.val() === '' || $search.val() === null) {
+        $search.addClass('is-invalid');
+      } return $searchVal = true;
+  }
+
+  function checkUrlValid() {
+    if($search.val().startsWith('https://') == false && $search.val().startsWith('http://') == false) {
+      $search.addClass('is-invalid');
+    }
+  }
+
+  //removes class after input is filled out
+  $search.click(removeInvalidClass);
+
+  function removeInvalidClass() {
+    if($searchVal) {
+      $search.removeClass('is-invalid');
+    }
+  }
 });
