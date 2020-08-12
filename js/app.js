bdaae3b0e444b8e93482005acb08f21209825247
Milos Radibratovic <radibratovic.milos@gmail.com>
2020-08-12 20:59:01 +0200

    Implement example AJAX request and refactor checkSearchEmpty function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+  const apiRootURL = 'https://rel.ink';
   const $search = $('#search');
 
   $('#submitBtn').click(function (e) {
@@ -7,12 +7,22 @@ $(document).ready(function () {
     checkSearchEmpty();
     checkUrlValid();
 
-    if (validation.$validUrl == true) {
-      //fetchUrls();
-      displayLinks();
+    if (validation.$validUrl && validation.$searchVal) {
+      $.ajax({
+        method: 'POST',
+        url: 'https://rel.ink/api/links/',
+        data: {
+          url: $search.val().trim()
+        },
+        dataType: "json"
+      })
+        .done((data, textStatus, jqXHR) => {
+          addLink(data.url, data.hashid);
+        });
     }
-  })
+  });
 
+  // there is no need to use $ in front object property names since they are not jQuery objects
   const validation = {
     $searchVal: false,
     $validUrl: false
@@ -21,15 +31,18 @@ $(document).ready(function () {
   function checkSearchEmpty() {
     if ($search.val() === '' || $search.val() === null) {
       $search.addClass('is-invalid');
-    } return validation.$searchVal = true;
+      validation.$searchVal = false;
+      return;
+    }
+    validation.$searchVal = true;
   }
 
+  // please refactor this using checkSearchEmpty function as a reference
   function checkUrlValid() {
     if ($search.val().startsWith('https://') == false && $search.val().startsWith('http://') == false) {
       $search.addClass('is-invalid');
     } return validation.$validUrl = true;
   }
-
 
   //removes class after input is filled out
   $search.click(removeInvalidClass);
@@ -41,34 +54,36 @@ $(document).ready(function () {
   }
 
   let $template = $('.template');
-  function displayLinks() {
+
+  function addLink(link, shortLinkHash) {
+    const shortLink = apiRootURL + '/' + shortLinkHash;
     $template.append(`
-        <div class="url-shorten-template">
-              <div class="url-shorten-template__col link-col">
-                <span class="url-shorten-template__link">${$('#search').val()}</span>
-              </div>
-              <div class="url-shorten-template__col short-link-col">
-                <a href="" class="url-shorten-template__short-link">https://rel.ink/</a>
-                <button class="url-shorten-template__btn" type="button">Copy</button>
-              </div>
-            </div>
-      `);
+      <div class="url-shorten-template">
+        <div class="url-shorten-template__col link-col">
+          <span class="url-shorten-template__link">${link}</span>
+        </div>
+        <div class="url-shorten-template__col short-link-col">
+          <a href="${shortLink}" target="_blank" class="url-shorten-template__short-link">${shortLink}</a>
+          <button class="url-shorten-template__btn" type="button">Copy</button>
+        </div>
+      </div>
+    `);
   }
 
   //post data
 
-   /* let link = {
-     search: $search.val()
-   }
+  /* let link = {
+    search: $search.val()
+  }
 
-  function fetchUrls() {
-    $.ajax({
-      type: 'POST',
-      url: 'https://rel.ink/api/links/',
-      data: link,
-    }).done(function(){
-      displayLinks();
-    });
-  } */
+ function fetchUrls() {
+   $.ajax({
+     type: 'POST',
+     url: 'https://rel.ink/api/links/',
+     data: link,
+   }).done(function(){
+     displayLinks();
+   });
+ } */
 
 });
